fb991beb56c59bfbc7f9609456f30e6fd1f350d4
s-hayashi123 <shake7602@gmail.com>
2025-10-18 22:29:25 +0900

    feat: ダッシュボードに食事追加機能を実装し、入力フォームを追加

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
 import type { MealRecord } from "@/db/schemas/meal";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Dashboard() {
   const [meals, setMeals] = useState<MealRecord[]>([]);
+  const [foodName, setFoodName] = useState("");
+  const [totalKcal, setTotalKcal] = useState("");
 
   const getMeals = async () => {
     const res = await fetch("/api/meal");
@@ -15,10 +19,39 @@ export default function Dashboard() {
     getMeals();
   }, []);
 
+  const addMeals = async () => {
+    await fetch("/api/meal", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ foodName, totalKcal: Number(totalKcal) }),
+    });
+    setFoodName("");
+    setTotalKcal("");
+    getMeals();
+  };
+
   return (
     <div className="container my-4">
       <h1 className="text-center font-bold text-3xl">ダッシュボード</h1>
       <h2 className="font-bold text-2xl">食事管理</h2>
+      <div className="flex space-x-3 my-4">
+        <Input
+          placeholder="食事名"
+          name="foodName"
+          value={foodName}
+          onChange={(e) => setFoodName(e.target.value)}
+        />
+        <Input
+          placeholder="カロリー数"
+          name="kcal"
+          value={totalKcal}
+          onChange={(e) => setTotalKcal(e.target.value)}
+        />
+        <Button onClick={addMeals}>追加</Button>
+      </div>
+
       <ul>
         {meals.map((meal) => (
           <li key={meal.id}>
---
+++ src/db/migrations/0004_overconfident_sir_ram.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "meal_records" DROP COLUMN "meal_type";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "amount";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "unit";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "total_protein";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "total_fat";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "total_carbs";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "recorded_at";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "updated_at";
