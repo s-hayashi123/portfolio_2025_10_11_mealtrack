5e30daeea2da3f2675de94e0e188a94b2f487baf
s-hayashi123 <shake7602@gmail.com>
2025-10-20 21:54:07 +0900

    fix: セッション検証時の未認証リダイレクト処理を削除

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -2,11 +2,9 @@ import "server-only";
 
 import { headers } from "next/headers";
 import { auth } from "./auth";
-import { redirect } from "next/navigation";
 
 export const verifiSession = async () => {
   const session = await auth.api.getSession({ headers: await headers() });
-  if (!session) redirect("/signup");
   return session;
 };
 
