6c4ee8fe71560b1c7632ef3dce1bdf4e3dfb29c8
s-hayashi123 <shake7602@gmail.com>
2025-10-30 08:41:07 +0900

    feat: マイグレーションファイルに新しいジャーナルエントリを追加

---
+++ src/db/migrations/0006_goofy_jack_power.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "meal_records" DROP COLUMN "amount";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "unit";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "total_protein";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "total_fat";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "total_carbs";--> statement-breakpoint
+ALTER TABLE "meal_records" DROP COLUMN "note";
